739c21b43a69f1afe13294848ec6e505ec00d92d
Syed Faizan <syedmfaizan@yahoo.com>
2026-06-16 03:55:13 +0000

    Maintenance Mode

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+// src/middleware.js
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const isMaintenance =
+    process.env.MAINTENANCE_MODE === undefined ||
+    process.env.MAINTENANCE_MODE === "true";
+  const url = new URL(context.request.url);
+
+  // Prevent infinite loops by allowing requests to the maintenance page itself
+  // Also allow static assets (like images or CSS) to load normally
+  if (
+    isMaintenance &&
+    url.pathname !== "/maintenance" &&
+    !url.pathname.startsWith("/_astro")
+  ) {
+    return context.redirect("/maintenance", 307);
+  }
+
+  // Continue to the requested page normally if maintenance is off
+  return next();
+});
